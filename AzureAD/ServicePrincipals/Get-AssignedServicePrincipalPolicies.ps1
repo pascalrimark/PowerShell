a54627453c143552a320acb351be31cb47510696
Pascal Rimark <primark@united-internet.de>
2022-06-23 08:10:13 +0200

    added support for multiple assigned policies

--- AzureAD/ServicePrincipals/Get-AssignedServicePrincipalPolicies.ps1
+++ AzureAD/ServicePrincipals/Get-AssignedServicePrincipalPolicies.ps1
@@ -1,21 +1,38 @@
-### Get assigned AzureAD service Pincipal Policies that were assigned via Add-AzureADServicePrincipalPolicy
-
 Connect-AzureAD
 $resultList = [System.Collections.ArrayList]::new()
 $sps = Get-AzureADServicePrincipal -All $true
 foreach($sp in $sps) {
-    $policy = Get-AzureADServicePrincipalPolicy -Id $sp.ObjectId | select *
-    if($policy -ne $null) {
+    $policies = Get-AzureADServicePrincipalPolicy -Id $sp.ObjectId | Select-Object *
+    if($policies.Count -ne 0) {
+        Write-Host "Adding $($sp.ObjectId) ($($sp.DisplayName)) found Policy for this ServicePrincipal"
         $item = @{}
         $item["SPId"] = $sp.ObjectId
         $item["SPName"] = $sp.DisplayName
-        $item["SPPolicyId"] = $policy.id
-        $item["SPPolicyDisplayName"] = $policy.DisplayName
-        $item["SPPolicyIsOrganizationDefault"] = $policy.IsOrganizationDefault
-        $item["SPPolicyType"] = $policy.Type
-        $item["SPPolicyDefinition"] = $policy.Definition
-        Write-Host "Added $($sp.ObjectId) ($($sp.DisplayName))"
+        $policyCollection = [System.Collections.ArrayList]::new()
+        foreach($p in $policies) {
+            $policyItem = @{}
+            $policyItem["SPPolicyId"] = $p.id
+            $policyItem["SPPolicyDisplayName"] = $p.DisplayName
+            $policyItem["SPPolicyIsOrganizationDefault"] = $p.IsOrganizationDefault
+            $policyItem["SPPolicyType"] = $p.Type
+            $policyItem["SPPolicyDefinition"] = $p.Definition
+            Write-Host "Adding policy $($p.id) ($($p.DisplayName)) for ServicePrincipal $($sp.ObjectId)"
+            $policyCollection.add([PScustomObject]$policyItem) | out-null
+        }
+        $item["AssignedPolicies"] = $policyCollection
         $resultList.add([PScustomObject]$item) | out-null
     }
 }    
-return $resultList | fl
+return $resultList | Format-List
+
+<#
+Result:
+
+PS C:\WINDOWS> $resultList | fl
+
+SPName           : Airwatch
+AssignedPolicies : {@{SPPolicyDefinition=; SPPolicyType=; SPPolicyIsOrganizationDefault=; SPPolicyDisplayName=; SPPolicyId=}, @{SPPolicyDefinition=System.Collections.Generic.List`1[System.String]; SPPolicyType=TokenIssuancePolicy; 
+                   SPPolicyIsOrganizationDefault=False; SPPolicyDisplayName=TokenIssuancePolicy; SPPolicyId=755f199e-ed23-489a-ad98-6c263277fa62}}
+SPId             : 33119c1f-a625-47c9-a0f2-a9d04bb93f99
+
+#>
